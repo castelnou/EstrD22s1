f51ce9837cdb61f20f422bde2a995c0e41e82984
Miguel Petrillo <miguel.petrillo@gmail.com>
2022-04-13 01:04:56 -0300

    Update Practica1.hs
    Se suben las soluciones a los Issues.

--- Practica1.hs
+++ Practica1.hs
@@ -1,29 +1,29 @@
 -- 1 Numeros enteros. 
 -- 1.1 Defina las siguientes funciones:
 --a) sucesor :: Int -> Int
--- Dado un número devuelve su sucesor
+-- Dado un nÃºmero devuelve su sucesor
 
 sucesor :: Int -> Int
 sucesor x = x + 1
 
 
 --b) sumar :: Int -> Int -> Int
--- Dados dos números devuelve su suma utilizando la operación +.
+-- Dados dos nÃºmeros devuelve su suma utilizando la operaciÃ³n +.
 
 sumar :: Int -> Int -> Int
 sumar x y  = x  + y
 
 --c) divisionYResto :: Int -> Int -> (Int, Int)
---Dado dos números, devuelve un par donde la primera componente es la división del
---primero por el segundo, y la segunda componente es el resto de dicha división. Nota:
---para obtener el resto de la división utilizar la función mod :: Int -> Int -> Int,
+--Dado dos nÃºmeros, devuelve un par donde la primera componente es la divisiÃ³n del
+--primero por el segundo, y la segunda componente es el resto de dicha divisiÃ³n. Nota:
+--para obtener el resto de la divisiÃ³n utilizar la funciÃ³n mod :: Int -> Int -> Int,
 --provista por Haskell.
 
 divisionYResto :: Int -> Int -> (Int, Int)
 divisionYResto x y = ( div x y, mod x y )
 
 --d) maxDelPar :: (Int,Int) -> Int
---Dado un par de números devuelve el mayor de estos.
+--Dado un par de nÃºmeros devuelve el mayor de estos.
 
 maxDelPar :: (Int,Int) -> Int
 maxDelPar ( x, y ) = max x y
@@ -34,7 +34,7 @@ maxDelPar ( x, y ) = max x y
 --las siguientes funciones:
 data Dir = Norte | Sur | Este | Oeste 
 
---a) Dada una dirección devuelve su opuesta.
+--a) Dada una direcciÃ³n devuelve su opuesta.
 opuesto :: Dir -> Dir
 opuesto Norte = Sur
 opuesto Sur   = Norte
@@ -50,9 +50,9 @@ iguales Oeste Oeste = True
 iguales x y = False
 
 
---c) Dada una dirección devuelve su siguiente, en sentido horario, y suponiendo que no existe
---la siguiente dirección a Oeste. 
--- Precondición: El Oeste no tiene siguiente
+--c) Dada una direcciÃ³n devuelve su siguiente, en sentido horario, y suponiendo que no existe
+--la siguiente direcciÃ³n a Oeste. 
+-- PrecondiciÃ³n: El Oeste no tiene siguiente
 -- Parcial ya que no esta definida para el Oeste, y da error.
 
 siguiente :: Dir -> Dir
@@ -61,14 +61,14 @@ siguiente Este = Sur
 siguiente Sur = Oeste
 
 
---2. Definir el tipo de dato DiaDeSemana, con las alternativas Lunes, Martes, Miércoles, Jueves,
---Viernes, Sabado y Domingo. Supongamos que el primer día de la semana es lunes, y el último
+--2. Definir el tipo de dato DiaDeSemana, con las alternativas Lunes, Martes, MiÃ©rcoles, Jueves,
+--Viernes, Sabado y Domingo. Supongamos que el primer dÃ­a de la semana es lunes, y el Ãºltimo
 --es domingo. Luego implementar las siguientes funciones:
 
 data DiaDeSemana = Lunes | Martes | Miercoles | Jueves | Viernes | Sabado | Domingo 
 
---a) Devuelve un par donde la primera componente es el primer día de la semana, y la
---segunda componente es el último día de la semana.
+--a) Devuelve un par donde la primera componente es el primer dÃ­a de la semana, y la
+--segunda componente es el Ãºltimo dÃ­a de la semana.
 
 primeroYUltimoDia :: (DiaDeSemana, DiaDeSemana)
 primeroYUltimoDia = (Lunes, Domingo)
@@ -83,17 +83,22 @@ empiezaConM Viernes = False
 empiezaConM Sabado = False
 empiezaConM Domingo = False
 
---c) Dado dos dias de semana, indica si el primero viene después que el segundo.
-vieneDespues :: DiaDeSemana -> DiaDeSemana -> Bool
-vieneDespues Martes Lunes  	  = True
-vieneDespues Miercoles Martes  = True
-vieneDespues Jueves Miercoles = True
-vieneDespues Viernes Jueves = True
-vieneDespues Sabado Viernes = True
-vieneDespues Domingo Sabado = True
-vieneDespues Lunes Domingo = True
-vieneDespues x y = False
+--c) Dado dos dias de semana, indica si el primero viene despuÃ©s que el segundo.
 
+--Como no se puede utilizar el deriving Ord, Se crea una funiÃ³n que devuelva el indice del dÃ­a
+-- de la semana. De esa manera si puedo comparar con ">"
+--Dado un dia de la semana indica que orden tiene, siendo Lunes el primer dia
+numeroDelDia :: DiaDeSemana -> Int
+numeroDelDia Lunes = 1
+numeroDelDia Martes = 2
+numeroDelDia Miercoles = 3
+numeroDelDia Jueves = 4
+numeroDelDia Viernes = 5
+numeroDelDia Sabado = 6
+numeroDelDia Domingo = 7
+
+vieneDespues ::  DiaDeSemana ->  DiaDeSemana -> Bool
+vieneDespues dia1 dia2  = numeroDelDia(dia1) > numeroDelDia(dia2)
 
 --d) Dado un dia de la semana indica si no es ni el primer ni el ultimo dia.
 estaEnElMedio :: DiaDeSemana -> Bool
@@ -105,7 +110,7 @@ estaEnElMedio Sabado 	= True
 estaEnElMedio x			= False
 
 --a) Dado un booleano, si es True devuelve False, y si es False devuelve True.
---En Haskell ya está definida como not.
+--En Haskell ya estÃ¡ definida como not.
 negar :: Bool -> Bool
 negar True = False
 negar False = True
@@ -117,20 +122,25 @@ implica :: Bool -> Bool -> Bool
 implica True False = False
 implica x y = True
 
---c) Dados dos booleanos si ambos son True devuelve True, sino devuelve False.
---En Haskell ya está definida como &&.
+--c) Dados dos booleanos si ambos son True devuelve True, sino devuelve False. Esta
+--funciÃ³n debe ser tal que yTambien False (error "Mal") devuelva False.
+--En Haskell ya estÃ¡ definida como \&\&.
+yTambien :: Bool -> Bool -> Bool
+yTambien True True = True
+yTambien _ _ = False
 
-and :: Bool -> Bool -> Bool
-and True True = True
-and x y = False
+
 
 --d) Dados dos booleanos si alguno de ellos es True devuelve True, sino devuelve False.
--- En Haskell ya está definida como ||.
-or :: Bool -> Bool -> Bool
-or True False = True
-or False True = True
-or True True = True
-or x y = False
+--Esta funciÃ³n debe ser tal que oBien True (error "Mal") devuelva True.
+--En Haskell ya estÃ¡ deefinida como ||.
+oBien :: Bool -> Bool -> Bool
+oBien False False = False
+oBien _ _ = True
+--or True False = True
+--or False True = True
+--or True True = True
+--or x y = False
 
 
 --3. Registros
@@ -180,7 +190,7 @@ laQueEsMayor personaL personaR =
 
 -- 3.2 Pokemones
 --Definir los tipos de datos P okemon, como un T ipoDeP okemon (agua, fuego o planta) y un
---porcentaje de energía; y Entrenador, como un nombre y dos pokemones. Luego definir las
+--porcentaje de energÃ­a; y Entrenador, como un nombre y dos pokemones. Luego definir las
 --siguientes funciones:
 
 data TipoDePokemon = Fuego | Agua | Planta
@@ -194,7 +204,7 @@ poke2 = ConsPoke Planta 40
 entrenador1 = ConsEntr "ent1" poke1 poke2
 entrenador2 = ConsEntr "ent2" poke2 poke2
 
---Dados dos pokémon indica si el primero, en base al tipo, es superior al segundo. Agua
+--Dados dos pokÃ©mon indica si el primero, en base al tipo, es superior al segundo. Agua
 --supera a fuego, fuego a planta y planta a agua. Y cualquier otro caso es falso.
 esGanador :: TipoDePokemon -> TipoDePokemon -> Bool
 esGanador Agua Fuego = True
@@ -211,16 +221,30 @@ superaA poke1 poke2 = esGanador (tipoPokemon poke1) ( tipoPokemon poke2)
 --superaA ( ConsPoke n p a t ) ( ConsPoke n2 p2 a2 t2 ) = esGanador t t2 ESTO ESTA MAL
 
 
---Devuelve la cantidad de pokémon de determinado tipo que posee el entrenador.
-sumaUnoSiEsMismoTipo :: TipoDePokemon -> TipoDePokemon -> Int
-sumaUnoSiEsMismoTipo Agua Agua = 1
-sumaUnoSiEsMismoTipo Fuego Fuego = 1
-sumaUnoSiEsMismoTipo Planta Planta = 1
-sumaUnoSiEsMismoTipo x y = 0
+--Devuelve la cantidad de pokÃ©mon de determinado tipo que posee el entrenador.
+--sumaUnoSiEsMismoTipo :: TipoDePokemon -> TipoDePokemon -> Int
+--sumaUnoSiEsMismoTipo Agua Agua = 1
+--sumaUnoSiEsMismoTipo Fuego Fuego = 1
+--sumaUnoSiEsMismoTipo Planta Planta = 1
+--sumaUnoSiEsMismoTipo x y = 0
 
+--Devuelve 1 si es True, sino 0
+unoSiEsTrue :: Bool -> Int
+unoSiEsTrue True = 1
+unoSiEsTrue False = 0
 
+--Devuelve True si dado 2 tipos de pokemon son iguales
+esDeMismoTipo :: TipoDePokemon -> TipoDePokemon -> Bool
+esDeMismoTipo Agua Agua = True
+esDeMismoTipo Fuego Fuego = True
+esDeMismoTipo Planta Planta = True
+esDeMismoTipo _ _ = False	
+
+--Se reemplaza sumaUnoSiEsMismoTipo por dos funciones con tareas mas especificas
 cantidadDePokemonesDe :: TipoDePokemon -> Entrenador -> Int
-cantidadDePokemonesDe t ( ConsEntr n poke1 poke2 ) =    sumaUnoSiEsMismoTipo t ( tipoPokemon poke1 )  +  sumaUnoSiEsMismoTipo t ( tipoPokemon poke2 ) 
+--cantidadDePokemonesDe t ( ConsEntr n poke1 poke2 ) =    sumaUnoSiEsMismoTipo t ( tipoPokemon poke1 )  +  sumaUnoSiEsMismoTipo t ( tipoPokemon poke2 ) 
+cantidadDePokemonesDe t ( ConsEntr n poke1 poke2 ) =    unoSiEsTrue ( esDeMismoTipo  t ( tipoPokemon poke1 )  )
+													+  unoSiEsTrue ( esDeMismoTipo  t ( tipoPokemon poke2 ) )
 
 --Dado un par de entrenadores, devuelve a sus pokemones en una lista.
 pokemonesEntrenador :: Entrenador -> [Pokemon]
@@ -230,13 +254,13 @@ juntarPokemones :: (Entrenador, Entrenador) -> [Pokemon]
 juntarPokemones ( entrenadorL, entrenadorR ) = pokemonesEntrenador entrenadorL ++ pokemonesEntrenador entrenadorR
 
 
---4. Funciones polimórficas
---1.Defina las siguientes funciones polimórficas: 
---a) Dado un elemento de algún tipo devuelve ese mismo elemento. 
+--4. Funciones polimÃ³rficas
+--1.Defina las siguientes funciones polimÃ³rficas: 
+--a) Dado un elemento de algÃºn tipo devuelve ese mismo elemento. 
 loMismo :: a -> a
 loMismo x = x
 
---b) Dado un elemento de algún tipo devuelve el número 7.
+--b) Dado un elemento de algÃºn tipo devuelve el nÃºmero 7.
 siempreSiete :: a -> Int
 siempreSiete x = 7
 
@@ -245,7 +269,7 @@ swap :: (a,b) -> (b, a)
 swap ( x , y ) = ( y , x )
 
 
---2. Dada una lista de elementos, si es vacía devuelve True, sino devuelve False.
+--2. Dada una lista de elementos, si es vacÃ­a devuelve True, sino devuelve False.
 -- Definida en Haskell como null.
 estaVacia :: [a] -> Bool
 estaVacia [] = True
@@ -259,7 +283,8 @@ elPrimero (x:_) =  x
 
 --Dada una lista devuelve esa lista menos el primer elemento.
 sinElPrimero :: [a] -> [a]
-sinElPrimero [] = []
+--sinElPrimero [] = []
+sinElPrimero [] = error "No hay elementos"
 sinElPrimero (x:xs) = xs
 
 --Dada una lista devuelve un par, donde la primera componente es el primer elemento de la
@@ -271,19 +296,3 @@ splitHead xs = (elPrimero xs, sinElPrimero xs)
 elUltimo :: [a] -> a
 elUltimo [x]                =  x
 elUltimo (_:xs)             =  elUltimo xs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
